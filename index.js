c5e8b6449ede17bfcec4ee01fec5adb32f0d5ec2
Ricardo Gómez <B05Fvsvl04@users.noreply.github.com>
2024-06-09 11:51:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const app = require("./server");
 
 mongoose
-  .connect("mongodb://localhost:27017/JestDB", { useNewUrlParser: true })
+  .connect("mongodb://testing:testing@localhost:27017/JestDB", { useNewUrlParser: true })
   .then(() => {
     app.listen(5000, () => {
       console.log("Server has started!");
