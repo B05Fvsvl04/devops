e9187cb4655cb43e262159b83aec79e16657515b
Ricardo Gómez <B05Fvsvl04@users.noreply.github.com>
2024-06-10 21:06:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ mongoose
       console.log("Server has started!");
     });
   });
+
+function calcularPromedio(numeros) {
+  // No se valida la entrada de datos
+  if (numeros.length === 0) {
+    return 0;
+  }
+
+  var suma = 0;
+  for (var i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+  }
+
+  // Se utiliza una variable no declarada
+  promedio = suma / numeros.length;
+  return promedio;
+}
+
+// Ejemplo de uso
+var resultado = calcularPromedio([10, 5, 20]);
+console.log("El promedio es:", resultado);
